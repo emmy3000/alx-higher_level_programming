bc0e8eebfde126f2f63fc0293ffae55b6ebee921
emmy3000 <emodiemeka@gmail.com>
2023-03-07 19:48:28 -0800

    function that prints Fizz for multiples of 3, Buzz for multiples of 5 and FizzBuzz for both 3 and 5 then numbers 1 to 100 if FizzBuzz numbers are absent

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-def fizzbuzz(number):
-    for number in range(100):
-        if number % 3 == 0 and number % 5 == 0:
+def fizzbuzz():
+    for i in range(100):
+        if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=' ')
-        elif number % 3 == 0:
+        elif i % 3 == 0:
             print("Fizz", end=' ')
-        elif number % 5 == 0:
+        elif i % 5 == 0:
             print("Buzz", end=' ')
         else:
-            print(number, end=' ')
+            print(i, end=' ')
