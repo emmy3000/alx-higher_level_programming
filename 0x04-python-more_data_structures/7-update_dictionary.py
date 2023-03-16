db0e4dd66dd070b1c179bacbdc34c99f29ee05d6
emmy3000 <emodiemeka@gmail.com>
2023-03-16 12:56:42 -0700

    Task Updated

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 """Program that replaces or adds key/value in a dictionary"""
 
 
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
-    return (a_dictionary)
+    return a_dictionary
