4b3aa24d6d0d0979005e1132e19fce449ffa4892
emmy3000 <emodiemeka@gmail.com>
2023-03-28 19:58:27 -0700

    Private instance attribute: size, with reference to task 0

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""class definition of a Square
+
+    size: a private attribute of class
+    Square.
+"""
+
+class Square:
+    def __init__(self, size=None):
+        self._size = size
