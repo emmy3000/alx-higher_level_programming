2a4ddc8b90740fa7c99c823a6773884e553fb747
emmy3000 <emodiemeka@gmail.com>
2023-03-29 13:02:07 -0700

    Initialize and Error check value assigned to class private attribute

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,26 +1,22 @@
 #!/usr/binpython3
 
-"""Defining a class Square."""
+"""class definition of a Square"""
 
 
 class Square:
-    """class level"""
+    """class Square's class level"""
 
     def __init__(self, size=0):
-        """Attributes for allocating sizes to
-        an instance.
+        """Initialize a protected attribute.
+        Args:
+            size(int): size of new Square instance.
 
-        size(int): integer size of a new square
-                instance.
-
-        Errors: TypeError message if value isn't
-                an integer.
-                ValueError message if value is less
-                than 0.
+        Errors:
+            TypeError message if value isn't an integer.
+            ValueError message if value is less than 0.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
