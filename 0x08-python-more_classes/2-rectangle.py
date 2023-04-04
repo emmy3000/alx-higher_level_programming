ae67fc81ef8c59c897bce3a44e92ae436189154c
emmy3000 <emodiemeka@gmail.com>
2023-04-03 19:04:09 -0700

    function for obtaining the area and perimeter of the rectangle

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+
+"""
+Module: ``2-rectangle.py``
+
+"""
+
+
+class Rectangle:
+    """class Rectangle definition"""
+    def __init__(self, width=0, height=0):
+        """
+        Initialize a width and height attributes with
+        optional integer literals.
+
+        Args:
+            width(int): width of the rectangle.
+            height(int): height of the rectangle.
+
+        Returns:
+            new instance of a class Rectangle.
+        """
+
+        self.__height = height
+        self.__width = width
+
+    @property
+    def width(self):
+        """object method for geting width private attribute"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set new width value"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """object method for getting height private attribute"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set new height value"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height  must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """method for obtaining the area of the rectangle"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """method for obtaining the perimeter of the retangle"""
+        return (self.__width + self.__height) * 2
