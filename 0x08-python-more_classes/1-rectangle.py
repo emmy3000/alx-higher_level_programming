c3a4005a1e656e8d7bb60e927b4e83e9d03221fd
emmy3000 <emodiemeka@gmail.com>
2023-04-03 18:24:14 -0700

    class Rectangle with default values for width and height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -31,7 +31,7 @@ class Rectangle:
         @width.setter
         def width(self, value):
             """set new width"""
-            if not isinstance(value, int):
+            if type(value) is not int:
                 raise TypeError("height must be an integer")
             if value < 0:
                 raise ValueError("height must be >= 0")
@@ -44,7 +44,7 @@ class Rectangle:
         @height.setter
         def height(self, value):
             """set new height"""
-            if not isinstance(value, int):
+            if type(value) is not int:
                 raise TypeError("height must be an integer")
             if value < 0:
                 raise ValueError("height  must be >= 0")
