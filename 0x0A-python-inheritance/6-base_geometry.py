6bd9144edbdf48dc333ab9b2ea54b28ef50d1dda
emmy3000 <emodiemeka@gmail.com>
2023-04-12 01:06:03 -0700

    definition of a public instance method used for raising Exceptions in the class BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/urs/bin/python3
+
+"""
+    Module: ``6-base_geometry.py``
+"""
+
+
+class BaseGeometry:
+    """A class with a public instance method for raising an Exception"""
+
+    def area(self):
+        """Public instance method that raises an Exception."""
+        raise Exception("area() is not implemented")
