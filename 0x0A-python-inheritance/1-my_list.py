cac7abd6b1cb35cb5c9115cadd0baf3378e0c7cf
emmy3000 <emodiemeka@gmail.com>
2023-04-11 23:32:42 -0700

    A class MyList exhibiting inheritance from its Base class list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,30 +1,15 @@
 #usr/bin/python3
 
-"""
-Module: `1-my_list.py`
-A module that defines a class MyList that inherits from list.
-"""
+"""Module: 1-my_list.py"""
 
 
 class MyList(list):
-    """
-    Derived class from base class named list
-    """
+    """Derived class from base class named list"""
 
     def __init__(self, data=[]):
-        """
-        Constructor for MyList class.
-
-        Args:
-            data(list): list of integers.
-        """
+        """Constructor for MyList class"""
         super().__init__(data)
 
     def print_sorted(self):
-        """
-        prints a sorted list of integers
-
-        Returns:
-            None
-        """
+        """prints a sorted list of integers"""
         print(sorted(self))
