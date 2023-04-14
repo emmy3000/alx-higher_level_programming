9acde752f18dd2433113461b76df55a723408bb3
emmy3000 <emodiemeka@gmail.com>
2023-04-13 17:15:16 -0700

    function that writes a string to a text file (UTF8) and returns the number of characters written

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+Module: 1-write_file
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    function writes text to a file encoded in UTF-8
+
+    Args:
+        filename(str): file to be written to
+        text(str): text to be written into filename
+
+    Return: number of characters written to filename
+    """
+
+    with open(filename, 'w', encoding="utf-8") as mexFile:
+        return mexFile.write(text)
