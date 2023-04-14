ecce39c30f0951c3a4f301d42d29e9791ce59658
emmy3000 <emodiemeka@gmail.com>
2023-04-13 17:43:22 -0700

    function that returns the JSON representation of an object (string)

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+Module: 4-from_json_string-input_output
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """
+    Returns an object represented by a JSON string
+
+    Args:
+        my_str(str): A JSON string
+
+    Return:
+        A Python data structure represented by the JSON string
+    """
+    return json.loads(my_str)
