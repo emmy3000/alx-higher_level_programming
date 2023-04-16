fd44cf3dc2960b8d5ec77e1d86391f803e3d76a0
emmy3000 <emodiemeka@gmail.com>
2023-04-15 18:58:04 -0700

    Add Base class and corresponding test case to module\n\nThis commit adds the Base class, which provides a way to create objects with unique\nIDs. It also includes a test case to ensure that the class behaves as expected in\nvarious scenarios.

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""Unittest for base module"""
+
+import unittest
+from models.base import Base
+
+import sys
+sys.path.append('../models')
+
+
+class TestBase(unittest.TestCase):
+    """Tests for Base class"""
+
+    def test_creation(self):
+        """Test for creating an instance of Base"""
+        b1 = Base()
+        self.assertIsInstance(b1, Base)
+        self.assertEqual(b1.id, 1)
+
+        b2 = Base()
+        self.assertIsInstance(b2, Base)
+        self.assertEqual(b2.id, 2)
+
+    def test_creation_with_id(self):
+        """Test for creating an instance of Base with an id"""
+        b1 = Base(10)
+        self.assertIsInstance(b1, Base)
+        self.assertEqual(b1.id, 10)
+
+        b2 = Base()
+        self.assertIsInstance(b2, Base)
+        self.assertEqual(b2.id, 3)
