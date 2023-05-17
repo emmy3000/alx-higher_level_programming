5b3c70190409c83ddec72c8ddc3e41fd13969e56
emmy3000 <emodiemeka@gmail.com>
2023-05-16 21:05:06 -0700

    Script imports temperature data and display average temperature by city in a database's table

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,7 @@
 -- displays cities with their temperatures measured in Fahrenheit.
 -- temperature values will be listed in a descending order
 
-SELECT `city`, AVG(value) AS `average_temperature`
+SELECT `city`, AVG(value) AS `avg_temp`
 FROM `temperatures`
 GROUP BY `city`
-ORDER BY `average_temperature` DESC;
+ORDER BY `avg_temp` DESC;
