09ae84aa5bcf638a03dda8be4ceb4d9c39d6b97b
emmy3000 <emodiemeka@gmail.com>
2023-05-16 19:28:24 -0700

    Script calculates an average and stores the value in a column created on the fly in a database's table in MySQL server

---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,5 @@
+-- computes the average score of all records in `second_table` of `hbtn_0c_0` database.
+-- result column will be named `average`
+
+SELECT AVG(`score`) AS `average`
+FROM `second_table`;
