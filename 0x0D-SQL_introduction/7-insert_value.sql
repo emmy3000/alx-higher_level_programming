dfeb9d88eeed1c710f35cc4b066bac94ce023706
emmy3000 <emodiemeka@gmail.com>
2023-05-16 18:37:30 -0700

    Script that inserts a new row in a table's database in MySQL server

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,7 @@
+-- inserts a new row in the table `first_table` (`hbtn_0c_0) in MySQL server
+-- new row would contain `id = 89` & `name = Best School`
+
+INSERT INTO `first_table` 
+	(`id`, `name`) 
+VALUES 
+	(89, 'Best School');
