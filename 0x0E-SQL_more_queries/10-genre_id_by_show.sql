659389bda821f0242c21eb719097122e31e96fea
emmy3000 <emodiemeka@gmail.com>
2023-05-17 19:35:35 -0700

    Add SQL script to list shows with linked genres in hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,8 +2,6 @@
 -- each record displays `tv_shows.title` - `tv_show_genres.genre.id`
 -- results are sorted in an ascending order by `tv_shows.title` and `tv_show_genre.genre_id`
 
-USE `hbtn_0d_tvshows`;
-
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
