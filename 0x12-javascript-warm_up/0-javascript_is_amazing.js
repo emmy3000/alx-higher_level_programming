ed765b9dc270e538d983a9977b60d825862b9e0e
username <email@gmail.com>
2023-06-13 14:53:52 -0700

    script prints a string to output

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 /* script that prints “JavaScript is amazing” */
 
-const MyVar = 'Javascript is amazing';
-console.log (MyVar);
+const myVar = 'Javascript is amazing';
+console.log(myVar);
