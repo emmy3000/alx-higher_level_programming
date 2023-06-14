5c83e1922fd1307d2b0aeb147ffe42bb93c7179c
username <email@gmail.com>
2023-06-14 16:31:43 -0700

    Defining a class that inherits certain attributes and methods from another class

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+ * Defines a class Square inheriting features from class Rectangle.
+ */
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Square;
