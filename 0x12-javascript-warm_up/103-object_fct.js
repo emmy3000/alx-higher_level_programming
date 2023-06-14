6c9e75325590d054807fe0c8307be5682c9165af
username <email@gmail.com>
2023-06-13 17:56:26 -0700

    Updated script to add a new function on execution increments an integer

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -7,13 +7,14 @@
 
 const myObject = {
   type: 'object',
-  value: 12,
-  incr: function incr () {
-    this.value++;
-  }
+  value: 12
+};
+console.log(myObject);
+
+myObject.incr = function () {
+  this.value++;
 };
 
-console.log(myObject);
 myObject.incr();
 console.log(myObject);
 myObject.incr();
