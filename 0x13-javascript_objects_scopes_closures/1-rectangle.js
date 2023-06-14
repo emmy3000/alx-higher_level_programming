f9ee3da2514c66e1b15144cd3ef04bb74adc5407
username <email@gmail.com>
2023-06-14 15:46:15 -0700

    Class constructor definition built to take two arguments

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/* class constructor definition taking 2 args for width & height */
+
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
