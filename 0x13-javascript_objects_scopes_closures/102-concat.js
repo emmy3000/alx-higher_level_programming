16834dfd820a8f3a65184b2713c7a77e38dca618
username <email@gmail.com>
2023-06-14 18:43:31 -0700

    Script concats two files and directs its result into a destination file

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -9,39 +9,10 @@
 
 const fs = require('fs');
 
-/* Check if all command-line arguments are provided */
-if (process.argv.length !== 5) {
-  console.error('Usage: node concat.js <file1> <file2> <destination>');
-  process.exit(1);
-}
-
-const file1 = process.argv[2];
-const file2 = process.argv[3];
+const sourceFile1 = process.argv[2];
+const sourceFile2 = process.argv[3];
 const destination = process.argv[4];
 
-/* Read the contents of the first file */
-fs.readFile(file1, 'utf8', (err, data1) => {
-  if (err) {
-    console.error(`Error reading ${file1}: ${err.message}`);
-    process.exit(1);
-  }
-
-  /* Read the contents of the second file */
-  fs.readFile(file2, 'utf8', (err, data2) => {
-    if (err) {
-      console.error(`Error reading ${file2}: ${err.message}`);
-      process.exit(1);
-    }
-
-    /* Concatenate the contents of the two files with a newline */
-    const concatenatedData = `${data1.trim()}\n${data2.trim()}\n`;
-
-    /* Write the concatenated data to the destination file */
-    fs.writeFile(destination, concatenatedData, 'utf8', (err) => {
-      if (err) {
-        console.error(`Error writing to ${destination}: ${err.message}`);
-        process.exit(1);
-      }
-    });
-  });
-});
+const content =
+  fs.readFileSync(sourceFile1, 'utf-8') + fs.readFileSync(sourceFile2, 'utf-8');
+fs.writeFileSync(destination, content, { encoding: 'utf-8' });
