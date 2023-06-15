f3055cf4dfa4ee09975e7f172ff1447f19edea55
username <email@gmail.com>
2023-06-14 17:50:45 -0700

    Defining a function that creates two lists imported from a module

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+/*
+ * script imports an array & computes a new array.
+ * The map() method must be utilized in this implementation.
+ * A new list must be created with each value equal to the initial list,
+ * multiplied by the index in the list.
+ *
+ */
+
+const list = require('./100-data').list;
+
+const newList = list.map((value, index) => value * index);
+
+console.log('Initial List:', list);
+console.log('New List:', newList);
