ec331e6d572eae3ceea930840c1d4f31d62315cf
username <email@gmail.com>
2023-06-18 20:28:47 -0700

    Update MySQL filter script for retrieving states by name

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,44 +1,61 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
-Module: 2-my_filter_states
-Description: Retrieves and displays all matching states from the states table
-             in the hbtn_0e_0_usa database.
-Usage:     ./2-my_filter_states.py <mysql_username> \
-                                <mysql_password> \
-                                <database_name> \
-                                <state_name_searched>
-"""
-import sys
-import MySQLdb
+This script connects to a MySQL server and displays the first
+row in the states table where the name matches the provided argument.
 
-if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+Usage:
+    ./2-my_filter_states.py <mysql_username> \
+                            <mysql_password> \
+                            <database_name> \
+                            <state_name_searched>
+"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    """
+    Main function of the script.
+    """
+    # Retrieve command line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
     state_name = sys.argv[4]
 
+    # Connect to MySQL server
     db = MySQLdb.connect(
-        user=username,
-        port=3306,
         host="localhost",
-        passwd=password,
-        db=database
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
     )
+
+    # Create a cursor object to execute SQL queries
     cursor = db.cursor()
 
+    # Create and execute the SQL query
     query = """
         SELECT *
         FROM states
-        WHERE name LIKE '{:s}'
+        WHERE name = %s
         ORDER BY id ASC
-    """.format(state_name)
+        LIMIT 1
+    """
+    cursor.execute(query, (state_name,))
 
-    cursor.execute(query)
-    matching_states = cursor.fetchall()
+    # Fetch the first row returned by the query
+    row = cursor.fetchone()
 
-    for state in matching_states:
-        if state[1] == state_name:
-            print(state)
+    # Display the result
+    print(row)
 
+    # Close the cursor and the database connection
     cursor.close()
     db.close()
+
+
+if __name__ == "__main__":
+    main()
