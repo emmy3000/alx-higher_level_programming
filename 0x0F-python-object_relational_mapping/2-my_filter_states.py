23de59b1c0972dc5fd3e15ff17a140c6c0a68205
username <email@gmail.com>
2023-06-18 19:47:52 -0700

    Update MySQL filter script for retrieving states by name

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+"""
+Module: 2-my_filter_states
+Description: Connects to a MySQL server and retrieves states from a database
+             based on the provided state name argument.
+"""
+
+import sys
+import MySQLdb
+
+
+def filter_states(username, password, database, state_name):
+    """
+    Connects to a MySQL server and retrieves states from the specified database
+    where the name matches the provided state_name argument.
+
+    Args:
+        username (str): The username for the MySQL server.
+        password (str): The password for the MySQL server.
+        database (str): The name of the database.
+        state_name (str): The name of the state to search for.
+
+    Returns:
+        None
+    """
+    try:
+        connection = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+
+        cursor = connection.cursor()
+
+        sql = """
+            SELECT id, name
+            FROM states
+            WHERE name = '{}'
+            ORDER BY id ASC
+            LIMIT 1
+        """.format(state_name)
+
+        cursor.execute(sql)
+
+        rows = cursor.fetchall()
+
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print(f"Error connecting to MySQL server: {e}")
+        sys.exit(1)
+
+    finally:
+        if connection:
+            connection.close()
+
+
+def main():
+    """
+    Entry point of the script.
+    Parses command-line arguments and calls the filter_states function.
+    """
+    if len(sys.argv) != 5:
+        print("Usage: python3 2-my_filter_states.py <username> <password> "
+              "<database> <state_name>")
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    filter_states(username, password, database, state_name)
+
+
+if __name__ == "__main__":
+    main()
