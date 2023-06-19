0af47d4f9ddf85726c25be9e7a7671b75dadf153
username <email@gmail.com>
2023-06-19 07:16:24 -0700

    Implement script to list states starting with 'N' without duplicates

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -17,12 +17,10 @@ def main():
     """
     Main function of the script.
     """
-    # Retrieve command line arguments
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
-    # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -31,10 +29,8 @@ def main():
         db=database_name
     )
 
-    # Create a cursor object to execute SQL queries
     cursor = db.cursor()
 
-    # Create and execute the SQL query
     query = """
         SELECT id, name
         FROM states
@@ -42,25 +38,18 @@ def main():
         ORDER BY id ASC
     """
     cursor.execute(query)
-
-    # Fetch all the rows returned by the query
     rows = cursor.fetchall()
-
-    # Create a set to store unique values
     unique_rows = set()
-
-    # Filter out duplicate rows
     filtered_rows = []
+
     for row in rows:
         if row not in unique_rows:
             filtered_rows.append(row)
             unique_rows.add(row)
 
-    # Display the results
     for row in filtered_rows:
         print(row)
 
-    # Close the cursor and the database connection
     cursor.close()
     db.close()
 
