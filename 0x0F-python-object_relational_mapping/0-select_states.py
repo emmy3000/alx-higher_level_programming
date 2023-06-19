b6cc78d2c69ba92156d21221113858e8b588af66
username <email@gmail.com>
2023-06-19 07:11:11 -0700

    Implement script to retrieve all states from the ‘hbtn_0e_0_usa’ database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This script retrieves all states from the hbtn_0e_0_usa database.
+This script retrieves all states from the `hbtn_0e_0_usa` database.
 
 Usage: python3 get_states.py <mysql_username> <mysql_password> <database_name>
 
@@ -11,55 +11,31 @@ displays the states in ascending order by their ID.
 import MySQLdb
 import sys
 
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-def retrieve_states(mysql_user, mysql_password, database_name):
-    """
-    Retrieve all unique states from the hbtn_0e_0_usa database.
-
-    Args:
-        mysql_user (str): MySQL username.
-        mysql_password (str): MySQL password.
-        database_name (str): Name of the database.
-
-    Returns:
-        list: List of tuples representing the retrieved states.
-    """
+    # Connect to MySQL server
     db = MySQLdb.connect(
             host="localhost",
             port=3306,
-            user=mysql_user,
-            passwd=mysql_password,
-            db=database_name
+            user=username,
+            passwd=password,
+            db=database
             )
 
+    # Create a cursor object to interact with the database
     cursor = db.cursor()
 
+    # Execute the SQL query to fetch states
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
+    # Fetch all the rows and print them
     rows = cursor.fetchall()
-
     for row in rows:
         print(row)
 
+    # Close the cursor and database connection
     cursor.close()
     db.close()
-
-    return unique_states
-
-
-def main():
-    """
-    Entry point of the script.
-    """
-    mysql_user = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    states = retrieve_states(mysql_user, mysql_password, database_name)
-
-    for state in states:
-        print(state)
-
-
-if __name__ == "__main__":
-    main()
