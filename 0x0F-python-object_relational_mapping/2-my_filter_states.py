7c047cedfb0abcf61a801bb6e4b692288b26350b
username <email@gmail.com>
2023-06-19 08:45:15 -0700

    Refactor MySQL script to retrieve all matching rows by state name.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -18,13 +18,11 @@ def main():
     """
     Main function of the script.
     """
-    # Retrieve command line arguments
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
     state_name = sys.argv[4]
 
-    # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -33,26 +31,20 @@ def main():
         db=database_name
     )
 
-    # Create a cursor object to execute SQL queries
     cursor = db.cursor()
 
-    # Create and execute the SQL query
-    query = """
-        SELECT *
-        FROM states
-        WHERE name LIKE %s
+    sql = """
+        SELECT id, name FROM states
+        WHERE name = BINARY '{}'
         ORDER BY id ASC
-    """
-    cursor.execute(query, (state_name,))
+    """.format(state_name)
+    cursor.execute(sql)
 
-    # Fetch all the rows returned by the query
     rows = cursor.fetchall()
 
-    # Display the results
     for row in rows:
         print(row)
 
-    # Close the cursor and the database connection
     cursor.close()
     db.close()
 
