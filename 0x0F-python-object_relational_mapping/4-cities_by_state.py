54cba38723018e83441566eee427fff04be00f62
username <email@gmail.com>
2023-06-19 11:19:53 -0700

    Connect a MySQL server and lists cities from a specified database.

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
 This script connects to a MySQL server and lists all cities
 from the specified database.
@@ -17,12 +17,10 @@ def main():
     """
     Main function of the script.
     """
-    # Retrieve command line arguments
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
 
-    # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -31,10 +29,8 @@ def main():
         db=database_name
     )
 
-    # Create a cursor object to execute SQL queries
     cursor = db.cursor()
 
-    # Create and execute the SQL query
     query = """
         SELECT cities.id, cities.name, states.name
         FROM cities
@@ -44,14 +40,11 @@ def main():
     """
     cursor.execute(query)
 
-    # Fetch all the rows returned by the query
     rows = cursor.fetchall()
 
-    # Display the results
     for row in rows:
         print(row)
 
-    # Close the cursor and the database connection
     cursor.close()
     db.close()
 
