a26f06b880413d0b5adca9299e2aa2b629fdf5f8
Emeka Emodi <emodiemeka@gmail.com>
2023-07-02 15:22:31 -0700

    Script retrieves response body size in bytes with curl

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 ################################################################################
 # Script: 0-body_size.sh
-# Description: This script sends a request to a specific URL using curl and
+# Description: This script sends a HTTP request to a specific URL using curl and
 #		displays the size of the response body in bytes.
 ################################################################################
 
-curl -s "$1" | wc -c
+url="$1"
+
+curl -s "$url" | wc -c
