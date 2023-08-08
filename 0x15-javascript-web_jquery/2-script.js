e59f3a63dd114921e2177e111eb20a401269b834
Emeka Emodi <emodiemeka@gmail.com>
2023-08-07 19:55:19 -0700

    feat: Update header text color to red on clicking DIV#red_header using jQuery

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/**
+ * Update the text color of the <header> element to red (#FF0000)
+ * when the user clicks on the tag DIV#red_header.
+ */
+$(document).ready(function() {
+	$('#red_header').click(function() {
+		$('header').css('color', '#FF0000');
+	});
+});
