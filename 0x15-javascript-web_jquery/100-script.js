41f72de0d2488f2d43447f16aeb47bd001982e6a
Emeka Emodi <emodiemeka@gmail.com>
2023-08-07 22:32:41 -0700

    feat: Update <header> text color to red using document.querySelector

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/**
+ * Updates the text color of the <header> element to red (#FF0000).
+ */
+document.addEventListener('DOMContentLoaded', function () {
+  const headerElement = document.querySelector('header');
+  headerElement.style.color = '#FF0000';
+});
