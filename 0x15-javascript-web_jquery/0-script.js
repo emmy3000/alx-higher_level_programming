a496d7361e6a9c2ef5cc90fc47db16d14763d5f4
Emeka Emodi <emodiemeka@gmail.com>
2023-08-07 19:37:31 -0700

    feat: Set header text color to red

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/**
+ * Automatically update the text color of the <header> element to red (#FF0000).
+ */
+var headerElement = document.querySelector('header');
+headerElement.style.color = '#FF0000';
