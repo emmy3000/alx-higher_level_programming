0d58321d524943e479ad8fccad8cffb5560b49da
Emeka Emodi <emodiemeka@gmail.com>
2023-08-07 22:28:14 -0700

    feat: Fetch and display translated 'hello' from URL

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,9 @@
+/**
+ * Fetches translation of "hello" from a specific URL and displays it in the DIV#hello tag.
+ */
+const $ = window.$;
+$(document).ready(function () {
+  $.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
+    $('#hello').text(data.hello);
+  });
+});
